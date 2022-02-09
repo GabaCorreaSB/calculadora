eee833d522c31dd346a62bb3c4080821469bfcba
GabaCorreaSB <gabacorrea@protonmail.com>
2022-02-09 16:58:31 -0300

    Huge Changes

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,12 +1,13 @@
-function conta(){
+function soma(){
+
     let a = document.getElementById("numero_1");
     let b = document.getElementById("numero_2");
-    let operador = document.getElementById("operador");
     let resposta = document.getElementById("resultado");
      
     let conta = "";
     let value_a = Number(a.value);
     let value_b = Number(b.value);
+    
     
     // && AND
     // || OR
@@ -21,39 +22,9 @@ function conta(){
         return;
     }
     
-    var operadores = ['+', '-', '/', '*', 'x', 'X', '%'];
-    var operator = operador.value;
-    const isInArray = operadores.includes(operator)
 
-
-    if (!(isInArray)){
-        alert("Operador inválido, coloque um dos operadores válidos ' +, -, *, x ou / ' Obrigado !");
-        return;
-    }
-
-    switch (operator){
-        case '+':
-            conta = value_a + value_b;
-            break;
-        case '-':
-            conta = value_a - value_b;
-            break;
-        case '/':
-            value_b === 0 ? alert('Não é possível divisão por zero!') : conta = value_a  / value_b;
-            break;
-        case '%':
-            value_b === 0 ? alert('Não é possível divisão por zero!') : conta = value_a  / value_b;
-            break;
-        case 'X':
-            conta = value_a * value_b;
-            break;
-        case '*':
-            conta = value_a * value_b;
-            break;
-        case 'x':
-            conta = value_a * value_b;
-            break;
-    }
+    conta = value_a + value_b
+ 
     var check = conta - Math.floor(conta) !== 0;
 
     if (check){
@@ -62,4 +33,116 @@ function conta(){
         resposta.value = conta;
     }
     
-}
+    }
+
+function minus(){
+
+    let a = document.getElementById("numero_1");
+    let b = document.getElementById("numero_2");
+    let resposta = document.getElementById("resultado");
+     
+    let conta = "";
+    let value_a = Number(a.value);
+    let value_b = Number(b.value);
+    
+    
+    // && AND
+    // || OR
+    // != é diferente
+    // !== é diferente e verifico o tipo
+    // = change evry
+    // == igual
+    // === igual e verfic type
+
+    if (Number.isNaN(value_a) || Number.isNaN(value_b)) {
+        alert("Não é um número, favor coloque um número ai bro !");
+        return;
+    }
+    
+
+    conta = value_a - value_b
+ 
+    var check = conta - Math.floor(conta) !== 0;
+
+    if (check){
+        resposta.value = conta.toFixed(2);
+    } else {
+        resposta.value = conta;
+    }
+    
+    }
+
+function times(){
+
+    let a = document.getElementById("numero_1");
+    let b = document.getElementById("numero_2");
+    let resposta = document.getElementById("resultado");
+     
+    let conta = "";
+    let value_a = Number(a.value);
+    let value_b = Number(b.value);
+    
+    
+    // && AND
+    // || OR
+    // != é diferente
+    // !== é diferente e verifico o tipo
+    // = change evry
+    // == igual
+    // === igual e verfic type
+
+    if (Number.isNaN(value_a) || Number.isNaN(value_b)) {
+        alert("Não é um número, favor coloque um número ai bro !");
+        return;
+    }
+    
+
+    conta = value_a * value_b
+ 
+    var check = conta - Math.floor(conta) !== 0;
+
+    if (check){
+        resposta.value = conta.toFixed(2);
+    } else {
+        resposta.value = conta;
+    }
+    
+    }
+
+function divide(){
+
+    let a = document.getElementById("numero_1");
+    let b = document.getElementById("numero_2");
+    let resposta = document.getElementById("resultado");
+     
+    let conta = "";
+    let value_a = Number(a.value);
+    let value_b = Number(b.value);
+    
+    
+    // && AND
+    // || OR
+    // != é diferente
+    // !== é diferente e verifico o tipo
+    // = change evry
+    // == igual
+    // === igual e verfic type
+
+    if (Number.isNaN(value_a) || Number.isNaN(value_b)) {
+        alert("Não é um número, favor coloque um número ai bro !");
+        return;
+    }
+    
+    value_b === 0 ? alert("Não pode dividir por zero!") : conta = value_a / value_b
+ 
+    var check = conta - Math.floor(conta) !== 0;
+
+    if (check){
+        resposta.value = conta.toFixed(2);
+    } else {
+        resposta.value = conta;
+    }
+    
+    }
+
+
